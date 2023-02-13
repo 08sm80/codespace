d9f1ca223a8271db021b7fe6c74c1f9b3b60d9df
Sagar <code@dailyascent.net>
2023-02-13 16:03:23 +0000

    Mon, Feb 13, 2023, 4:03 PM +00:00

--- sagar/hello.c
+++ sagar/hello.c
@@ -3,6 +3,6 @@
 
 int main(void)
 {
-    string name = get_string("What is your name? ");
-    printf("hello, %s \n", name);
+    int name = get_string("What is your name? ");
+    printf("hello, %i \n", name);
 }
