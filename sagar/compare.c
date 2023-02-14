484490e33bb80cc82fa33de5cf12f9060f23e49b
Sagar <code@dailyascent.net>
2023-02-14 18:53:22 +0000

    Tue, Feb 14, 2023, 6:53 PM +00:00

--- sagar/compare.c
+++ sagar/compare.c
@@ -6,13 +6,13 @@ int main(void)
     int x = get_int("What is the value of x? ");
     int y = get_int("What is the value of y? ");
 
-    if (x > y)
-    {
+ /*   if (x > y)
+   {
         printf("X is the greater number");
     }
     if else (x < y)
     {
         printf("Y is the greater number");
     }
-    else (printf("X and Y are equal"));
+    else (printf("X and Y are equal"));*/
 }
