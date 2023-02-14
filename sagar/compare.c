7a6c4f9954dcbc08c6e6aa0f69216f8360326911
Sagar <code@dailyascent.net>
2023-02-14 19:00:57 +0000

    Tue, Feb 14, 2023, 7:00 PM +00:00

--- sagar/compare.c
+++ sagar/compare.c
@@ -3,15 +3,19 @@
 
 int main(void)
 {
-    int x = get_int("What is the value of x? ");
-    int y = get_int("What is the value of y? ");
+    int x = get_int("What is the value of X? ");
+    int y = get_int("What is the value of Y? ");
 
     if (x < y)
     {
         printf("X is less than Y \n");
     }
-    else if
+    else if (x > y)
     {
         printf("X is greater than Y \n");
+    }
+    else (x = y)
+    {
+        printf("There must be a Z somewhere we don't know yet \n");
     }
 }
